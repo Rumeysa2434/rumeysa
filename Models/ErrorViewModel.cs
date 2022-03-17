7d1733b84c502ab9fd47bef4879e8f07578cb1d7
Rumeysa2434 <101821826+Rumeysa2434@users.noreply.github.com>
2022-03-17 20:32:33 +0300

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace rumeysa.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Models/rumeysa.cs
@@ -0,0 +1,5 @@
+public class a{
+     public int sifre{get;set;}
+      public bool hesap{get;set;}
+          public DateTime yayıntarihi { get; set; }
+}
